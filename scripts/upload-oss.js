b63c934e8ed1f4409459e3b5a0ad23cb35a31c2c
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-04-02 23:30:30 +0800

    chore: 上传 oss 前对模板排序&合并 bizcharts 数据

--- scripts/upload-oss.js
+++ scripts/upload-oss.js
@@ -1,5 +1,8 @@
 const oss = require('ali-oss');
 const path = require('path');
+const fs = require('fs');
+const request = require('request');
+const scaffolds = require('./scaffolds');
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
@@ -21,11 +24,85 @@ const ossClient = oss({
   time: '120s',
 });
 
-const fromPath = path.resolve(__dirname, '../build/materials.json');
+const materialPath = path.resolve(__dirname, '../build/materials.json');
 const toPath = path.join(assetsPath, 'react-materials.json');
 
-console.log('start upload oss', fromPath, toPath);
+console.log('start upload oss', materialPath, toPath);
 
-ossClient.put(toPath, fromPath).then((result) => {
-  console.log('upload success', result);
-});
+sortScaffoldMaterials()
+  .then(mergeBizchartsBlocks)
+  .then(() => {
+    return ossClient.put(toPath, materialPath);
+  })
+  .then((result) => {
+    console.log('upload success', result);
+  });
+
+/**
+ * 按照下载量进行排序推荐
+ */
+function sortScaffoldMaterials() {
+  return new Promise((resolve, reject) => {
+    const materialsData = JSON.parse(fs.readFileSync(materialPath, 'utf-8'));
+
+    const sortMaterialsData = [];
+    scaffolds.forEach((scaffold) => {
+      materialsData.scaffolds.forEach((currentItem) => {
+        if (currentItem.name === scaffold) {
+          sortMaterialsData.push(currentItem);
+        }
+      });
+    });
+
+    materialsData.scaffolds = sortMaterialsData;
+
+    return fs.writeFile(
+      materialPath,
+      JSON.stringify(materialsData, null, 2),
+      'utf-8',
+      (err) => {
+        if (err) reject(err);
+        resolve();
+      }
+    );
+  });
+}
+
+
+/**
+ * 合并 bizchart 的区块物料
+ */
+function mergeBizchartsBlocks() {
+  return new Promise((resolve, reject) => {
+    const materialsData = JSON.parse(fs.readFileSync(materialPath, 'utf-8'));
+
+    request(
+      {
+        url:
+          'http://g.alicdn.com/bizcharts-material/scripts/material-assets.json',
+        json: true,
+      },
+      (error, response, body) => {
+        if (body) {
+          const bizchartBlocks = (body.blocks || []).map((item) => {
+            item.categories = ['图表'];
+            return item;
+          });
+          materialsData.blocks = materialsData.blocks.concat(bizchartBlocks);
+          return fs.writeFile(
+            materialPath,
+            JSON.stringify(materialsData, null, 2),
+            'utf-8',
+            (err) => {
+              if (err) reject(err);
+              resolve();
+            }
+          );
+        }
+
+        console.log('获取 bizcharts 物料源失败', error);
+        return resolve();
+      }
+    );
+  });
+}
