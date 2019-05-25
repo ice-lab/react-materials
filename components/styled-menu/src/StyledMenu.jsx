ed62b4f0545b745a5abfafd873e21fd710f23fef
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-05-25 13:35:33 +0800

    fix: 修复依赖&移除 flow 语法

--- components/styled-menu/src/StyledMenu.jsx
+++ components/styled-menu/src/StyledMenu.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 
 // import for the iconfont arrow-down
 // do not delete this because of the css iconfont
-import FoundationSymbol from 'foundation-symbol'; // eslint-disable-line
+import FoundationSymbol from '@icedesign/foundation-symbol'; // eslint-disable-line
 
 // selectedKeys openKeys props 不要随便设置默认值，否则可能会触发一些值获取逻辑从而出现问题
 
@@ -147,7 +147,7 @@ export default class IceMenu extends Component {
     }
   };
 
-  handleOpenChange = (openKeys: string[]) => {
+  handleOpenChange = (openKeys) => {
     this.setOpenKeys(openKeys);
 
     const { onOpenChange } = this.props;
