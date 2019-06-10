d6923c9e05bc424902239c56e58851ebb49c0d48
xinpeng.gxp <jasonintju@gmail.com>
2019-06-10 17:38:24 +0800

    refactor: BrandShowCase CardItems CardList

--- blocks/BrandShowCase/src/index.jsx
+++ blocks/BrandShowCase/src/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid } from '@alifd/next';
-
+import styles from './index.module.scss';
 const { Row, Col } = Grid;
 
 const generatorData = (count) => {
@@ -26,19 +26,19 @@ export default class BrandShowCase extends Component {
   render() {
     const data = generatorData(12);
     return (
-      <div className="brand-show-case" style={styles.container}>
-        <div style={styles.head}>
-          <h2 style={styles.title}>品牌展示</h2>
-          <p style={styles.intro}>
+      <div className="brand-show-case" className={styles.container}>
+        <div className={styles.head}>
+          <h2 className={styles.title}>品牌展示</h2>
+          <p className={styles.intro}>
             广义的“品牌”是具有经济价值的无形资产，用抽象化的、特有的、能识别的心智概念来表现其差异性，从而在人们的意识当中占据一定位置的综合反映。品牌建设具有长期性
           </p>
         </div>
-        <Row gutter="20" wrap style={styles.items}>
+        <Row gutter="20" wrap className={styles.items}>
           {data.map((item, index) => {
             return (
               <Col xxs="12" s="4" l="4" key={index}>
-                <div style={styles.item}>
-                  <img src={item.imgUrl} style={styles.image} alt="" />
+                <div className={styles.item}>
+                  <img src={item.imgUrl} className={styles.image} alt="" />
                 </div>
               </Col>
             );
@@ -48,36 +48,3 @@ export default class BrandShowCase extends Component {
     );
   }
 }
-
-const styles = {
-  container: {
-    width: '100%',
-    maxWidth: '1080px',
-    margin: '0 auto',
-    padding: '80px 0',
-  },
-  head: {
-    width: '80%',
-    maxWidth: '540px',
-    margin: '0 auto',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: '28px',
-  },
-  intro: {
-    textAlign: 'center',
-    color: '#999',
-  },
-  items: {
-    margin: '30px 0',
-  },
-  item: {
-    margin: '10px 0',
-    textAlign: 'center',
-  },
-  image: {
-    width: '100px',
-    height: '80px',
-  },
-};
