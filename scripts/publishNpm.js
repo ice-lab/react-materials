f7b52f1a1f9e79df5a0751eb55a7c7decd84ef99
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-06-12 12:10:28 +0800

    chore: build and publish scaffolds

--- scripts/publishNpm.js
+++ scripts/publishNpm.js
@@ -15,36 +15,30 @@ files.forEach((item) => {
   const pkgPath = path.resolve(dir, item);
   // eslint-disable-next-line import/no-dynamic-require
   const pkgData = require(pkgPath);
-
   const { name: npmName } = pkgData;
 
-  const npms = [
-    '@icedesign/asset-management-scaffold', '@icedesign/ecommerce-scaffold',
-    '@icedesign/pro-scaffold', '@icedesign/monitor-dashboard',
-    '@icedesign/project-management-admin', '@icedesign/scroll-screen-homepage-scaffold',
-    // '@icedesign/usertrack-admin-scaffold',
-  ];
+  console.log('start publish', npmName);
 
-  if (npms.indexOf(npmName) !== -1) {
-    console.log('start publish', npmName);
+  execSync('pwd', {
+    stdio: 'inherit',
+    cwd: dirpath,
+  });
+  execSync('tnpm update', {
+    stdio: 'inherit',
+    cwd: dirpath,
+  });
+  execSync('npm version patch', {
+    stdio: 'inherit',
+    cwd: dirpath,
+  });
+  execSync('npm publish', {
+    stdio: 'inherit',
+    cwd: dirpath,
+  });
+  execSync('tnpm sync', {
+    stdio: 'inherit',
+    cwd: dirpath,
+  });
 
-    execSync('pwd', {
-      stdio: 'inherit',
-      cwd: dirpath,
-    });
-    execSync('tnpm update', {
-      stdio: 'inherit',
-      cwd: dirpath,
-    });
-    execSync('npm publish', {
-      stdio: 'inherit',
-      cwd: dirpath,
-    });
-    execSync('tnpm sync', {
-      stdio: 'inherit',
-      cwd: dirpath,
-    });
-
-    console.log('publish success', npmName);
-  }
+  console.log('publish success', npmName);
 });
