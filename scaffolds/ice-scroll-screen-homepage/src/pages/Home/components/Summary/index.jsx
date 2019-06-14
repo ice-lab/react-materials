5f40d5f5d1cfa5b975e60634b22e03184c4b890d
xinpeng.gxp <jasonintju@gmail.com>
2019-06-14 11:52:34 +0800

    refactor: SSH jsx + css-modules

--- scaffolds/ice-scroll-screen-homepage/src/pages/Home/components/Summary/index.jsx
+++ scaffolds/ice-scroll-screen-homepage/src/pages/Home/components/Summary/index.jsx
@@ -1,41 +1,15 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const Summary = (props) => {
   return (
-    <div key="summary" style={styles.summary}>
-      <div style={styles.num}>{`0${props.num} _`}</div>
-      <div style={styles.title}>{props.title}</div>
-      <div style={styles.subtitle}>{props.subTitle}</div>
+    <div key="summary" className={styles.summary}>
+      <div className={styles.num}>{`0${props.num} _`}</div>
+      <div className={styles.title}>{props.title}</div>
+      <div className={styles.subtitle}>{props.subTitle}</div>
     </div>
   );
 };
 
-const styles = {
-  summary: {
-    marginRight: '100px',
-    display: 'inline-block',
-    float: 'left',
-  },
-  num: {
-    fontSize: '64px',
-    color: '#fff',
-    lineHeight: '56px',
-    marginBottom: '30px',
-    fontWeight: '700',
-  },
-  title: {
-    fontSize: '42px',
-    color: '#fff',
-    lineHeight: '42px',
-    marginBottom: '15px',
-    fontWeight: '700',
-  },
-  subtitle: {
-    fontSize: '20px',
-    color: '#fff',
-    lineHeight: '20px',
-    marginBottom: '15px',
-  },
-};
 
 export default Summary;
