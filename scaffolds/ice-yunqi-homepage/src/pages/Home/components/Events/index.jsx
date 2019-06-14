75f77062e16ffe2556c2730847f6ed8ae2342fc4
xinpeng.gxp <jasonintju@gmail.com>
2019-06-14 14:46:36 +0800

    refactor: YH jsx + css-modules

--- scaffolds/ice-yunqi-homepage/src/pages/Home/components/Events/index.jsx
+++ scaffolds/ice-yunqi-homepage/src/pages/Home/components/Events/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ScrollAnim from 'rc-scroll-anim';
 import QueueAnim from 'rc-queue-anim';
+import styles from './index.module.scss';
 
 const ScrollOverPack = ScrollAnim.OverPack;
 
@@ -18,19 +19,19 @@ export default class Events extends Component {
 
   render() {
     return (
-      <div style={styles.container}>
+      <div className={styles.container}>
         <ScrollOverPack always={false}>
           <QueueAnim type="bottom" delay={100} duration={1000}>
-            <div style={styles.content} key="content">
-              <div style={styles.summary}>
-                <div style={styles.mainTitle}>EVENTS</div>
-                <div style={styles.mainDesc}>创新活动</div>
+            <div className={styles.content} key="content">
+              <div className={styles.summary}>
+                <div className={styles.mainTitle}>EVENTS</div>
+                <div className={styles.mainDesc}>创新活动</div>
               </div>
-              <div style={styles.items}>
-                <div style={{ ...styles.item, ...styles.itemFull }}>
-                  <div style={{ ...styles.itemFullBox, ...styles.itemBox }}>
-                    <div style={styles.subtit}>YunQi GeekPai 云栖极客派</div>
-                    <div style={styles.desc}>
+              <div className={styles.items}>
+                <div className={styles.itemFull}>
+                  <div className={styles.itemFullBox}>
+                    <div className={styles.subtit}>YunQi GeekPai 云栖极客派</div>
+                    <div className={styles.desc}>
                       新大陆的登月者,
                       去探索未来世界的无限可能。GXIC2018，安全大赛，区块链活动，汇聚最geek的开发者；特设极客体验区，现场感受数据运算，8分钟成为contributor，super
                       coder
@@ -38,21 +39,21 @@ export default class Events extends Component {
                     </div>
                   </div>
                 </div>
-                <div style={{ ...styles.item, ...styles.itemLeft }}>
-                  <div style={{ ...styles.itemLeftBox, ...styles.itemBox }}>
-                    <div style={styles.subtit}>Tech Go 云栖智能运动会</div>
-                    <div style={styles.desc}>
+                <div className={styles.itemLeft}>
+                  <div className={styles.itemLeftBox}>
+                    <div className={styles.subtit}>Tech Go 云栖智能运动会</div>
+                    <div className={styles.desc}>
                       运动和科技的结合，智能和肌肉一起发力，数据与肾上腺素同步共振。篮球、足球、跑步、自行车，在这里亲身体验前所未有的黑科技玩法。YOU
                       ARE TECH HERO！
                     </div>
                   </div>
                 </div>
-                <div style={{ ...styles.item, ...styles.itemRight }}>
-                  <div style={{ ...styles.itemRightBox, ...styles.itemBox }}>
-                    <div style={styles.subtit}>
+                <div className={styles.itemRight}>
+                  <div className={styles.itemRightBox}>
+                    <div className={styles.subtit}>
                       Music Festival 云栖·虾米音乐节
                     </div>
-                    <div style={styles.desc}>
+                    <div className={styles.desc}>
                       这是一场科技与音乐的碰撞、创新与传统的融合、潮流与经典的交锋。大牌汇聚，燃爆现场，辅助数字前沿科技呈现，奉上震撼持久的试听盛宴。云栖·虾米音乐节打造最热科技音乐现场，等你来撩！
                     </div>
                   </div>
@@ -65,98 +66,3 @@ export default class Events extends Component {
     );
   }
 }
-
-const styles = {
-  container: {
-    minHeight: '1000px',
-    padding: '50px 0',
-    background: '#000',
-  },
-  summary: {
-    width: '1200px',
-    margin: '0 auto',
-  },
-  mainTitle: {
-    fontSize: '60px',
-    color: '#fff',
-    letterSpacing: '0.77px',
-    lineHeight: '72px',
-    margin: '0',
-    fontWeight: '700',
-  },
-  mainDesc: {
-    fontSize: '24px',
-    lineHeight: '30px',
-    color: '#fff',
-    marginTop: '8px',
-    fontWeight: '700',
-  },
-  items: {
-    marginTop: '70px',
-    overflow: 'hidden',
-  },
-  item: {
-    float: 'left',
-    height: '360px',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: '50%',
-    position: 'relative',
-  },
-  itemFull: {
-    width: '100%',
-    backgroundImage:
-      'url(https://img.alicdn.com/tfs/TB1Ylpwn5OYBuNjSsD4XXbSkFXa-2884-720.png)',
-    backgroundSize: 'cover',
-  },
-  itemLeft: {
-    width: '46%',
-    backgroundImage:
-      'url(https://img.alicdn.com/tfs/TB1SQ5cwXOWBuNjy0FiXXXFxVXa-1340-720.png)',
-    backgroundSize: 'cover',
-  },
-  itemRight: {
-    width: '54%',
-    backgroundSize: 'cover',
-    backgroundImage:
-      'url(https://img.alicdn.com/tfs/TB15sxxwgmTBuNjy1XbXXaMrVXa-1556-720.png)',
-  },
-  itemBox: {
-    position: 'absolute',
-    bottom: '0',
-    width: '100%',
-    height: '158px',
-    padding: '28px 90px',
-    background: 'rgba(0,0,0,.7)',
-    transition: 'all .3s',
-  },
-  itemFullBox: {
-    paddingRight: '39.5%',
-    left: '46%',
-    maxWidth: '54%',
-  },
-  itemLeftBox: {
-    paddingLeft: '39.5%',
-    right: 0,
-  },
-  itemRightBox: {
-    paddingRight: '39.5%',
-    left: 0,
-  },
-  subtit: {
-    fontSize: '20px',
-    lineHeight: '30px',
-    color: '#fff',
-    fontWeight: '700',
-  },
-  desc: {
-    fontSize: '14px',
-    lineHeight: '22px',
-    color: '#fff',
-    height: '66px',
-    marginTop: '6px',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    display: '-webkit-box',
-    WebkitLineClamp: '4',
-  },
-};
