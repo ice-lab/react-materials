73f29cb5e4685d63033a675fd96f498fe8017546
xinpeng.gxp <jasonintju@gmail.com>
2019-06-14 10:50:53 +0800

    refactor: OPL jsx + css-modules

--- scaffolds/ice-open-platform-landingpage/src/components/Footer/index.jsx
+++ scaffolds/ice-open-platform-landingpage/src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './index.module.scss';
 
 export default class FooterLinks extends Component {
   static displayName = 'FooterLinks';
@@ -14,33 +15,33 @@ export default class FooterLinks extends Component {
 
   render() {
     return (
-      <div style={styles.footerBox}>
-        <div style={styles.footerBoxContent}>
-          <div style={styles.footerClearfix} />
-          <div style={styles.footerBoxLeft}>
-            <ul style={styles.footerBoxContentUl}>
-              <li style={styles.footerBoxContentLi}>
-                <div style={styles.footerBoxContentItem}>
-                  <div style={styles.footerBoxContentName}>新手入门</div>
+      <div className={styles.footerBox}>
+        <div className={styles.footerBoxContent}>
+          <div className={styles.footerClearfix} />
+          <div className={styles.footerBoxLeft}>
+            <ul className={styles.footerBoxContentUl}>
+              <li className={styles.footerBoxContentLi}>
+                <div className={styles.footerBoxContentItem}>
+                  <div className={styles.footerBoxContentName}>新手入门</div>
                   <div>
                     <ul>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           新手指南
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           热门FAQ
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           合作攻略
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           平台规则
                         </a>
                       </li>
@@ -48,28 +49,28 @@ export default class FooterLinks extends Component {
                   </div>
                 </div>
               </li>
-              <li style={styles.footerBoxContentLi}>
-                <div style={styles.footerBoxContentItem}>
-                  <div style={styles.footerBoxContentName}>服务支持</div>
+              <li className={styles.footerBoxContentLi}>
+                <div className={styles.footerBoxContentItem}>
+                  <div className={styles.footerBoxContentName}>服务支持</div>
                   <div>
                     <ul>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           开发文档
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           业务文档
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           物料模板下载
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           开放日
                         </a>
                       </li>
@@ -78,18 +79,18 @@ export default class FooterLinks extends Component {
                 </div>
               </li>
 
-              <li style={styles.footerBoxContentLi}>
-                <div style={styles.footerBoxContentItem}>
-                  <div style={styles.footerBoxContentName}>客服帮助</div>
+              <li className={styles.footerBoxContentLi}>
+                <div className={styles.footerBoxContentItem}>
+                  <div className={styles.footerBoxContentName}>客服帮助</div>
                   <div>
                     <ul>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           技术支持中心
                         </a>
                       </li>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           在线问答
                         </a>
                       </li>
@@ -97,13 +98,13 @@ export default class FooterLinks extends Component {
                   </div>
                 </div>
               </li>
-              <li style={styles.footerBoxContentLi}>
-                <div style={styles.footerBoxContentItem}>
-                  <div style={styles.footerBoxContentName}>合作洽谈</div>
+              <li className={styles.footerBoxContentLi}>
+                <div className={styles.footerBoxContentItem}>
+                  <div className={styles.footerBoxContentName}>合作洽谈</div>
                   <div>
                     <ul>
-                      <li style={styles.footerBoxContentSubli}>
-                        <a href="#" style={styles.footerBoxContentItemLink}>
+                      <li className={styles.footerBoxContentSubli}>
+                        <a href="#" className={styles.footerBoxContentItemLink}>
                           点此与我联系
                         </a>
                       </li>
@@ -111,132 +112,34 @@ export default class FooterLinks extends Component {
                   </div>
                 </div>
               </li>
-              <li style={styles.footerClearfix} />
+              <li className={styles.footerClearfix} />
             </ul>
           </div>
-          <div style={styles.footerBoxRight}>
-            <div style={styles.footerBoxRightTitle}>关注我们</div>
+          <div className={styles.footerBoxRight}>
+            <div className={styles.footerBoxRightTitle}>关注我们</div>
             <div
-              style={{
-                ...styles.footerBoxRightItem,
-                ...styles.footerBoxRightItemOpacity,
-              }}
+              className={`${styles.footerBoxRightItem} ${styles.footerBoxRightItemOpacity}`}
             >
               加入钉钉群
             </div>
-            <div style={styles.footerBoxRightItem}>
+            <div className={styles.footerBoxRightItem}>
               <img
-                style={styles.footerBoxRightItemImg}
+                className={styles.footerBoxRightItemImg}
                 src={require('./images/TB1J2tnuKSSBuNjy0FlXXbBpVXa-1060-1086.png')}
                 alt=""
               />
             </div>
             <div
-              style={{
-                ...styles.footerBoxRightItem,
-                ...styles.footerBoxRightItemOpacity,
-              }}
+              className={`${styles.footerBoxRightItem} ${styles.footerBoxRightItemOpacity}`}
             >
               打开钉钉扫一扫
             </div>
           </div>
-          <div style={styles.footerClearfix} />
+          <div className={styles.footerClearfix} />
         </div>
       </div>
     );
   }
 }
 
-const styles = {
-  footerBox: {
-    backgroundColor: '#232940',
-    WebkitFontSmoothing: 'antialiased',
-    fontFamily: 'Tahoma,arial,Hiragino Sans GB,Microsoft Yahei',
-    lineHeight: '1.6',
-    boxSizing: 'border-box',
-    minWidth: '1280px',
-  },
-  footerBoxContent: {
-    width: '1240px',
-    margin: '0 auto',
-    padding: '50px 0px 30px',
-    boxSizing: 'border-box',
-  },
-  footerClearfix: {
-    display: 'block',
-    height: 0,
-    boxSizing: 'border-box',
-    clear: 'both',
-  },
-  footerBoxLeft: {
-    float: 'left',
-    width: '1020px',
-    boxSizing: 'border-box',
-  },
-  footerBoxRight: {
-    float: 'right',
-    width: '220px',
-    paddingLeft: '80px',
-    borderLeft: '1px solid rgba(151,151,151,0.4)',
-    boxSizing: 'border-box',
-  },
-  footerBoxContentUl: {
-    listStyle: 'none',
-    margin: '0',
-    padding: '0',
-    boxSizing: 'border-box',
-  },
-  footerBoxContentLi: {
-    listStyle: 'none',
-    marginLeft: '0',
-    boxSizing: 'border-box',
-    float: 'left',
-    width: '25%',
-    paddingRight: '10px',
-  },
-  footerBoxContentItem: {
-    width: '208px',
-    boxSizing: 'border-box',
-  },
-  footerBoxContentName: {
-    fontSize: '22px',
-    color: '#FFFFFF',
-    paddingBottom: '20px',
-    boxSizing: 'border-box',
-  },
-  footerBoxContentSubli: {
-    paddingTop: '14px',
-    whiteSpace: 'normal',
-    wordBreak: 'break-all',
-    overflow: 'hidden',
-    boxSizing: 'border-box',
-    listStyle: 'none',
-  },
-  footerBoxContentItemLink: {
-    fontSize: '16px',
-    color: '#FFFFFF',
-    opacity: '0.6',
-    textDecoration: 'none',
-  },
-  footerBoxRightTitle: {
-    paddingBottom: '16px',
-    fontSize: '22px',
-    color: '#FFFFFF',
-    boxSizing: 'border-box',
-  },
-  footerBoxRightItem: {
-    paddingTop: '12px',
-    fontSize: '14px',
-    color: '#FFFFFF',
-    boxSizing: 'border-box',
-  },
-  footerBoxRightItemOpacity: {
-    opacity: 0.6,
-  },
-  footerBoxRightItemImg: {
-    width: '97px',
-    height: '97px',
-    border: '0',
-    boxSizing: 'border-box',
-  },
-};
+
