28605d4f0e1d9006e9f43d015a1451628e4074f8
xinpeng.gxp <jasonintju@gmail.com>
2019-06-14 14:09:12 +0800

    refactor: UH jsx + css-modules

--- scaffolds/ice-universal-homepage/src/components/NotFound/index.jsx
+++ scaffolds/ice-universal-homepage/src/components/NotFound/index.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import IceContainer from '@icedesign/container';
-import './index.scss';
+import './NotFound.scss';
+import styles from './index.module.scss';
 
-export default class BasicNotFound extends Component {
-  static displayName = 'BasicNotFound';
+export default class Index extends Component {
+  static displayName = 'Index';
 
   render() {
     return (
       <div className="basic-not-found">
         <IceContainer>
-          <div style={styles.exceptionContent} className="exception-content">
+          <div className={`exception-content ${styles.exceptionContent}`}>
             <img
-              src={require('./images/notFound.png')}
-              style={styles.image}
+              src="https://img.alicdn.com/tfs/TB1txw7bNrI8KJjy0FpXXb5hVXa-260-260.png"
               className="imgException"
               alt="页面不存在"
             />
             <div className="prompt">
-              <h3 style={styles.title} className="title">
+              <h3 className={`${styles.title} title`} >
                 抱歉，你访问的页面不存在
               </h3>
-              <p style={styles.description} className="description">
-                您要找的页面没有找到，请返回<Link to="/">首页</Link>继续浏览
+              <p className={`${styles.description} description`} >
+                您要找的页面没有找到，请返回
+                <Link to="/">首页</Link>
+                继续浏览
               </p>
             </div>
           </div>
@@ -32,16 +34,4 @@ export default class BasicNotFound extends Component {
   }
 }
 
-const styles = {
-  exceptionContent: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    color: '#333',
-  },
-  description: {
-    color: '#666',
-  },
-};
+
