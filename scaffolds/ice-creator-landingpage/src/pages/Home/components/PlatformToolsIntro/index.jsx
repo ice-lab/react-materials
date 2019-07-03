d4c7eb8bef6bf3fa0d96b30946fd8bf32e84a0dd
jasonintju <jasonintju@gmail.com>
2019-07-03 11:34:28 +0800

    refactor: ice-creator-landingpage upgrade kit@2.x

--- scaffolds/ice-creator-landingpage/src/pages/Home/components/PlatformToolsIntro/index.jsx
+++ scaffolds/ice-creator-landingpage/src/pages/Home/components/PlatformToolsIntro/index.jsx
@@ -1,42 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './index.module.scss';
 
-export default class PlatformIntro2 extends Component {
-  static displayName = 'PlatformIntro2';
-
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div
-        className={styles.wrapper}
-        style={{
-          backgroundImage: `url(${require('./images/TB1d..oRVXXXXX4XVXXXXXXXXXX-2760-1480.png')})`,
-        }}
-      >
-        <div className={styles.body}>
-          <h2 className={styles.title}>高效的内容创作工具</h2>
-          <p className={styles.text}>
-            聚集全网最新鲜的创意，直击用户需求<br />海量优质素材，快速锁定优质商品、图片、视频<br />发布前内容质量诊断，内容编辑更高效
-          </p>
-        </div>
-        <img
-          src={require('./images/TB1DzIrRVXXXXckXFXXXXXXXXXX-1740-800.png')}
-          alt=""
-          width="870"
-          height="400"
-          className={styles.image}
-        />
-      </div>
-    );
-  }
-}
-
-
+export default () => (
+  <div
+    className={styles.wrapper}
+    style={{
+      backgroundImage: `url(${require('./images/TB1d..oRVXXXXX4XVXXXXXXXXXX-2760-1480.png')})`,
+    }}
+  >
+    <div className={styles.body}>
+      <h2 className={styles.title}>高效的内容创作工具</h2>
+      <p className={styles.text}>
+        聚集全网最新鲜的创意，直击用户需求<br />海量优质素材，快速锁定优质商品、图片、视频<br />发布前内容质量诊断，内容编辑更高效
+      </p>
+    </div>
+    <img
+      src={require('./images/TB1DzIrRVXXXXckXFXXXXXXXXXX-1740-800.png')}
+      alt=""
+      width="870"
+      height="400"
+      className={styles.image}
+    />
+  </div>
+);
