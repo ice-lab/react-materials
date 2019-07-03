9e6d9fddde7ddf0dcc0a29b5ed02f75322be7d49
jasonintju <jasonintju@gmail.com>
2019-07-03 12:42:02 +0800

    refactor: ice-scroll-screen-homepage upgrade kit@2.x

--- scaffolds/ice-scroll-screen-homepage/src/layouts/BlankLayout/index.jsx
+++ scaffolds/ice-scroll-screen-homepage/src/layouts/BlankLayout/index.jsx
@@ -1,37 +1,11 @@
-
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
 import Layout from '@icedesign/layout';
 import Footer from '../../components/Footer';
-import routerData from '../../routerConfig';
-import NotFound from '../../components/NotFound';
 import styles from './index.module.scss';
 
-export default class HeaderFooterLayout extends Component {
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  render() {
-    return (
-      <Layout className={styles.container}>
-        <Switch>
-          {routerData.map((item, index) => {
-            return item.component ? (
-              <Route
-                key={index}
-                path={item.path}
-                component={item.component}
-              />
-            ) : null;
-          })}
-
-          {/* 未匹配到的路由重定向到 NotFound */}
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </Layout>
-    );
-  }
-}
-
+export default props => (
+  <Layout className={styles.container}>
+    { props.children }
+    <Footer />
+  </Layout>
+);
