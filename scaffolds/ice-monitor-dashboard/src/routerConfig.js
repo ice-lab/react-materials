cf79d9ab2826fb843268a46f95dbd74cea99476f
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-03 18:40:37 +0800

    chore: 路由迁移

--- scaffolds/ice-monitor-dashboard/src/routerConfig.js
+++ scaffolds/ice-monitor-dashboard/src/routerConfig.js
@@ -1,49 +1,70 @@
-// 以下文件格式为描述路由的协议格式
-// 你可以调整 routerConfig 里的内容
-// 变量名 routerConfig 为 iceworks 检测关键字，请不要修改名称
+import BasicLayout from '@/layouts/BasicLayout';
+import UserLayout from '@/layouts/UserLayout';
+import Business from '@/pages/Business';
+import DataCenter from '@/pages/DataCenter';
+import TrafficStatistics from '@/pages/TrafficStatistics';
+import UserStatistics from '@/pages/UserStatistics';
+import UserActivities from '@/pages/UserActivities';
+import UserLogin from '@/pages/UserLogin';
+import UserRegister from '@/pages/UserRegister';
+import Setting from '@/pages/Setting';
+import NotFound from '@/components/NotFound';
 
-import Business from './pages/Business';
-import DataCenter from './pages/DataCenter';
-import TrafficStatistics from './pages/TrafficStatistics';
-import UserStatistics from './pages/UserStatistics';
-import UserActivities from './pages/UserActivities';
-import UserLogin from './pages/UserLogin';
-import UserRegister from './pages/UserRegister';
-import Setting from './pages/Setting';
-
-const routerConfig = [
-  {
-    path: '/account/login',
-    component: UserLogin,
-  },
-  {
-    path: '/account/register',
-    component: UserRegister,
-  },
-  {
-    path: '/dashboard',
-    component: Business,
-  },
-  {
-    path: '/traffic/statistics',
-    component: TrafficStatistics,
-  },
-  {
-    path: '/user/statistics',
-    component: UserStatistics,
-  },
-  {
-    path: '/user/activities',
-    component: UserActivities,
-  },
-  {
-    path: '/datacenter',
-    component: DataCenter,
-  },
-  {
-    path: '/setting',
-    component: Setting,
-  },
-];
+const routerConfig = [{
+  path: '/account',
+  children: [
+    {
+      path: '/login',
+      component: UserLogin,
+    },
+    {
+      path: '/register',
+      component: UserRegister,
+    },
+    {
+      path: '/',
+      redirect: '/account/login',
+    },
+    {
+      component: NotFound,
+    },
+  ]
+}, {
+  path: '/',
+  component: BasicLayout,
+  children: [
+    {
+      path: '/user/statistics',
+      component: UserStatistics,
+    },
+    {
+      path: '/user/activities',
+      component: UserActivities,
+    },
+    {
+      path: '/dashboard',
+      component: Business,
+    },
+    {
+      path: '/traffic/statistics',
+      component: TrafficStatistics,
+    },
+    {
+      path: '/datacenter',
+      component: DataCenter,
+    },
+    {
+      path: '/setting',
+      component: Setting,
+    },
+    {
+      path: '/',
+      redirect: '/dashboard',
+    },
+    {
+      component: NotFound,
+    },
+  ],
+}];
 
 export default routerConfig;
