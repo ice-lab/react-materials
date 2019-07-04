bbbf5c4c4e8ff4c78c4176731a7cb90a9d696fd3
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-07-05 00:33:58 +0800

    chore: 修正依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ const { eslint, deepmerge } = require('@ice/spec');
 
 module.exports = deepmerge(eslint, {
   rules: {
-    // 'react-hooks/rules-of-hooks': 0,
-    // 'no-unused-expressions': 0,
+    "global-require": 0,
   }
 });
