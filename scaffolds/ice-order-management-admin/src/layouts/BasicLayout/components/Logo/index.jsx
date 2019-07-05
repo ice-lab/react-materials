87e29bafa73c1cb84f0dc64e786a60ecb4715c55
boiawang <zhengwang2314@gmail.com>
2019-07-05 15:41:45 +0800

    chore: style fix

--- scaffolds/ice-order-management-admin/src/layouts/BasicLayout/components/Logo/index.jsx
+++ scaffolds/ice-order-management-admin/src/layouts/BasicLayout/components/Logo/index.jsx
@@ -6,7 +6,7 @@ import styles from './index.module.scss';
 export default function Logo({ style, text }) {
   return (
     <div className={styles.container} style={style}>
-      <Link to="/" className="logoText">
+      <Link to="/" className={styles.logoText}>
         {text || 'LOGO'}
       </Link>
     </div>
