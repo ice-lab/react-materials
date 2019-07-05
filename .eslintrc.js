4927db5ccf53eb898311c7572f2c221df3a61d3e
boiawang <zhengwang2314@gmail.com>
2019-07-05 11:55:14 +0800

    chore: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,5 @@ const { eslint, deepmerge } = require('@ice/spec');
 module.exports = deepmerge(eslint, {
   rules: {
     "global-require": 0,
-    "import/prefer-default-export": 0,
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-    "no-else-return": 0,
   }
 });
