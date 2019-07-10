a60c5435fbe99996ef64ffc9b9f6e8be8a993deb
daysai <470040845@qq.com>
2019-07-10 14:54:06 +0800

    feat: rm react-router-dom

--- scaffolds/ice-stark-layout/src/index.jsx
+++ scaffolds/ice-stark-layout/src/index.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // 载入默认全局样式 normalize 、.clearfix 和一些 mixin 方法等
 import '@alifd/next/reset.scss';
 
-import router from './router';
+import BasicLayout from '@/layouts/BasicLayout';
+import Index from '@/pages/Index';
 
 const ICE_CONTAINER = document.getElementById('ice-container');
 
@@ -11,4 +13,9 @@ if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
 
-ReactDOM.render(router(), ICE_CONTAINER);
+ReactDOM.render(
+  <BasicLayout>
+    <Index />
+  </BasicLayout>,
+  ICE_CONTAINER
+);
--- scaffolds/ice-stark-layout/src/layouts/BasicLayout/components/Logo/index.jsx
+++ scaffolds/ice-stark-layout/src/layouts/BasicLayout/components/Logo/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
 export default function Logo() {
-  return <Link to="/" className={styles.logo} />;
+  return <div to="/" className={styles.logo} />;
 }
