7aad7e4572a25c2d10a19ddc690efabf3e22bda5
daysai <470040845@qq.com>
2019-07-17 19:03:59 +0800

    feat: update child assests

--- scaffolds/ice-stark-layout/src/pages/Index/index.jsx
+++ scaffolds/ice-stark-layout/src/pages/Index/index.jsx
@@ -19,34 +19,21 @@ export default function BasicPage() {
         path={['/', '/list', '/detail']}
         basename="/"
         exact
-        title="主页"
-        url={['//localhost:4444/js/index.js', '//localhost:4444/css/index.css']}
+        title="商家平台"
+        url={[
+          '//g.alicdn.com/icestark-demo/child/0.2.1/js/index.js',
+          '//g.alicdn.com/icestark-demo/child/0.2.1/css/index.css',
+        ]}
       />
       <AppRoute
-        path="/waiter"
-        basename="/waiter"
-        title="用户页面"
-        url={['//localhost:5555/js/index.js', '//localhost:5555/css/index.css']}
+        path="/user"
+        basename="/user"
+        title="小二平台"
+        url={[
+          '//g.alicdn.com/icestark-demo/child2/0.2.0/js/index.js',
+          '//g.alicdn.com/icestark-demo/child2/0.2.0/css/index.css',
+        ]}
       />
-      {/* <AppRoute
-          path={['/', '/home', '/about']}
-          basename="/"
-          exact
-          title="主页"
-          url={[
-            '//g.alicdn.com/icestark-demo/child/0.1.2/js/index.js',
-            '//g.alicdn.com/icestark-demo/child/0.1.2/css/index.css',
-          ]}
-        />
-        <AppRoute
-          path="/user"
-          basename="/user"
-          title="用户页面"
-          url={[
-            '//g.alicdn.com/icestark-demo/child2/0.1.2/js/index.js',
-            '//g.alicdn.com/icestark-demo/child2/0.1.2/css/index.css',
-          ]}
-        /> */}
     </AppRouter>
   );
 }
