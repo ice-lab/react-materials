a3916b427de1b3a6c338b033edb62a1e3792c3e8
ClarkXia <xiawenwu41@gmail.com>
2019-07-25 20:45:34 +0800

    feat: add css-modules-typescript-loader for ts scaffold (#96)
    * feat: add css-modules-typescript-loader
    
    * chore: version

--- scaffolds/ice-typescript-starter/ice.config.js
+++ scaffolds/ice-typescript-starter/ice.config.js
@@ -14,5 +14,17 @@ module.exports = {
       locales: ['zh-cn'],
     }],
   ],
+  chainWebpack: (config) => {
+    // 修改对应 css module的 loader，默认修改 scss-module 同理可以修改 css-module 和 less-module 规则
+    ['scss-module'].forEach((rule) => {
+      if (config.module.rules.get(rule)) {
+        config.module.rule(rule).use('ts-css-module-loader')
+          .loader(require.resolve('css-modules-typescript-loader'))
+          .options({ modules: true, sass: true });
+        // 指定应用loader的位置
+        config.module.rule(rule).use('ts-css-module-loader').before('css-loader');
+      }
+    });
+  },
 };
 
