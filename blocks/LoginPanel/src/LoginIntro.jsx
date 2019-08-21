486ca00073c3db0755feabcc8ff8751d9f6e3bba
jasonintju <jasonintju@gmail.com>
2019-08-21 16:59:44 +0800

    feat: add LoginPanel

---
+++ blocks/LoginPanel/src/LoginIntro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+export default function LoginIntro() {
+  return (
+    <div className={styles.containerInfo}>
+      <div className={styles.contentInfo}>
+        <div className={styles.titleInfo}>优质服务，追求卓越</div>
+        <p className={styles.description}>Amazing Stuff is Lorem Here. ICE Team</p>
+      </div>
+      <div className={styles.mask} />
+    </div>
+  );
+}
---
+++ blocks/LoginPanel/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid } from '@alifd/next';
+import LoginIntro from './LoginIntro';
+import LoginForm from './LoginForm';
+import styles from './index.module.scss';
+
+const { Row, Col } = Grid;
+
+export default function Index() {
+  return (
+    <div className={styles.container}>
+      <Row wrap className={styles.row}>
+        <Col l="16">
+          <LoginIntro />
+        </Col>
+        <Col l="8">
+          <div className={styles.content}>
+            <LoginForm />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
