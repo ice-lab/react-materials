76f431a5087423df53a1a82a7b3ee65a5144f1e1
daysai <wddpwzz36@gmail.com>
2019-10-08 16:11:04 +0800

    feat: adjust to @ice/stark-app

--- scaffolds/ice-stark-child/src/index.jsx
+++ scaffolds/ice-stark-child/src/index.jsx
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom';
-import { getMountNode } from '@ice/stark';
+import { getMountNode, registerAppLeave } from '@ice/stark-app';
 
 // 载入默认全局样式 normalize
 import '@alifd/next/reset.scss';
 
 import router from './router';
+
+// make sure the unmount event is triggered
+registerAppLeave(() => {
+  ReactDOM.unmountComponentAtNode(getMountNode());
+});
 
 ReactDOM.render(router(), getMountNode());
