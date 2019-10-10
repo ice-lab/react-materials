9208a2e5bf77f1d6e0033a9fa6578f9534c58686
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-10 21:07:05 +0800

    chore: release npm

--- scaffolds/ice-stark-layout/src/App.jsx
+++ scaffolds/ice-stark-layout/src/App.jsx
@@ -37,39 +37,44 @@ export default class App extends React.Component {
           onAppLeave={this.handleAppLeave}
           onAppEnter={this.handleAppEnter}
         >
+          {/* 基于 React@15.x */}
           <AppRoute
             path={['/', '/message', '/about']}
             basename="/"
             exact
             title="通用页面"
             url={[
-              // '//g.alicdn.com/icestark-demo/child2/0.2.1/js/index.js',
-              // '//g.alicdn.com/icestark-demo/child2/0.2.1/css/index.css',
-              'http://localhost:5555/js/index.js',
-              'http://localhost:5555/css/index.css',
+              '//unpkg.com/icestark-child-common/build/js/index.js',
+              '//unpkg.com/icestark-child-common/build/css/index.css',
+
+              // 'http://localhost:5555/js/index.js',
+              // 'http://localhost:5555/css/index.css',
             ]}
           />
+
+          {/* 基于 React@16.x */}
           <AppRoute
             path="/seller"
             basename="/seller"
             title="商家平台"
             url={[
-              // '//g.alicdn.com/icestark-demo/child/0.2.1/js/index.js',
-              // '//g.alicdn.com/icestark-demo/child/0.2.1/css/index.css',
-              'http://localhost:4445/js/index.js',
-              'http://localhost:4445/css/index.css',
+              '//unpkg.com/icestark-child-seller/build/js/index.js',
+              '//unpkg.com/icestark-child-seller/build/css/index.css',
+              // 'http://localhost:6666/js/index.js',
+              // 'http://localhost:6666/css/index.css',
             ]}
           />
 
+          {/* 基于 Vue@2.x */}
           <AppRoute
             basename="/waiter"
             path="/waiter"
             title="小二平台"
             url={[
-              // '//g.alicdn.com/icestark-demo/child/0.2.1/js/index.js',
-              // '//g.alicdn.com/icestark-demo/child/0.2.1/css/index.css',
-              'http://localhost:8080/app.js',
-              'http://localhost:8080/css/app.css',
+              '//unpkg.com/icestark-child-waiter/dist/js/app.js',
+              '//unpkg.com/icestark-child-waiter/dist/css/app.css',
+              // 'http://localhost:8080/css/app.css',
+              // 'http://localhost:8080/app.js',
             ]}
           />
 
