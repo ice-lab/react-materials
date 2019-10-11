159eeb9956ec68846d48191db6c90ecbc3a369ef
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-11 17:55:36 +0800

    chore: optimize icestark child

--- scaffolds/ice-stark-child/src/index.jsx
+++ scaffolds/ice-stark-child/src/index.jsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom';
 import { getMountNode, registerAppLeave } from '@ice/stark-app';
 
-// 载入默认全局样式 normalize
-import '@alifd/next/reset.scss';
-
 import router from './router';
 
 // make sure the unmount event is triggered
--- scaffolds/ice-stark-child/src/layouts/BasicLayout/index.jsx
+++ scaffolds/ice-stark-child/src/layouts/BasicLayout/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LayoutTitle from '@/components/LayoutTitle';
-
 import style from './index.module.scss';
 
 export default props => {
   return (
     <div className={style.main}>
-      <LayoutTitle title={<Link to="/">商家平台</Link>} />
+      <h3>商家平台</h3>
       {props.children}
     </div>
   );
