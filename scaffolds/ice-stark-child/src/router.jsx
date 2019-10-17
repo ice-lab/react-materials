e3323fa9ac0016f4f56dff6860945ea6897243e7
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-17 21:07:24 +0800

    feat: optimize router.jsx

--- scaffolds/ice-stark-child/src/router.jsx
+++ scaffolds/ice-stark-child/src/router.jsx
@@ -5,11 +5,11 @@ import { getBasename } from '@ice/stark-app';
 import routes from '@/config/routes';
 
 const RouteItem = props => {
-  const { redirect, path: routePath, component, key } = props;
+  const { redirect, path: routePath, component, key, ...others } = props;
   if (redirect) {
-    return <Redirect exact key={key} from={routePath} to={redirect} />;
+    return <Redirect exact key={key} from={routePath} to={redirect} {...others} />;
   }
-  return <Route key={key} component={component} path={routePath} />;
+  return <Route key={key} component={component} path={routePath} {...others}/>;
 };
 
 export default () => {
@@ -18,36 +18,35 @@ export default () => {
       <Switch>
         {routes.map((route, id) => {
           const { component: RouteComponent, children, ...others } = route;
-          return (
-            <Route
-              key={id}
-              {...others}
-              component={props => {
-                return children ? (
-                  <RouteComponent key={id} {...props}>
-                    <Switch>
-                      {children.map((routeChild, idx) => {
-                        const { redirect, path: childPath, component } = routeChild;
-                        return RouteItem({
-                          key: `${id}-${idx}`,
-                          redirect,
-                          path: childPath && path.join(route.path, childPath),
-                          component,
-                        });
-                      })}
-                    </Switch>
-                  </RouteComponent>
-                ) : (
-                  <>
-                    {RouteItem({
-                      key: id,
-                      ...route,
-                    })}
-                  </>
-                );
-              }}
-            />
-          );
+          if (!children) {
+            return RouteItem({
+              key: id,
+              ...route,
+            });
+          } else {
+            return (
+              <Route
+                key={id}
+                {...others}
+                component={props => {
+                  return (
+                    <RouteComponent key={id} {...props}>
+                      <Switch>
+                        {children.map((routeChild, idx) => {
+                          const { path: childPath } = routeChild;
+                          return RouteItem({
+                            key: `${id}-${idx}`,
+                            ...routeChild,
+                            path: childPath && path.join(route.path, childPath)
+                          });
+                        })}
+                      </Switch>
+                    </RouteComponent>
+                  );
+                }}
+              />
+            );
+          }
         })}
       </Switch>
     </Router>
