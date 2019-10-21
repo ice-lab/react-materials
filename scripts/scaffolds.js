976426847b1f0e07300539d25e4a3d8202406588
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2019-10-21 14:31:46 +0800

    chore: ci

--- scripts/scaffolds.js
+++ scripts/scaffolds.js
@@ -2,52 +2,17 @@
  * 按照下载量和最新的规则对模板进行排序推荐
  */
 const scaffolds = [
-  // 基础模板(3)
-  'ice-design-pro',
-  'ice-design-lite',
-  'ice-typescript-starter',
+  // 基础模板
+  '@icedesign/pro-scaffold',
+  '@icedesign/lite-scaffold',
+  '@icedesign/ts-scaffold',
+  '@icedesign/ice-admin',
+  '@icedesign/ice-analysis',
+  '@icedesign/ice-homepage',
 
-  // 行业模板(22)
-  'ice-design-cms',
-  'ice-order-management-admin',
-  'ice-government-management-admin',
-  'ice-reviews-management',
-  'ice-monitor-dashboard',
-  'ice-project-management-admin',
-  'ice-application-management-admin',
-  'ice-commodity-management-admin',
-  'ice-operation-admin',
-  'ice-design-ecommerce',
-  'ice-voice-chat-admin',
-  'ice-hr-management-admin',
-  'ice-creator-admin',
-  'ice-library-management-admin',
-  'ice-asset-management-admin',
-  'ice-task-management-admin',
-  'ice-algorithm-model-admin',
-  'ice-contract-management-admin',
-  'ice-usertrack-admin',
-  'ice-builder-platform',
-  'ice-design-schools',
-  'ice-customer-analysis-admin',
-
-  // 数据可视化模板(3)
-  'ice-design-analysis',
-  'ice-finance-dashboard',
-  'ice-design-dashboard',
-
-  // 官网模板(7)
-  'ice-yunqi-homepage',
-  'ice-scroll-screen-homepage',
-  'ice-universal-homepage',
-  'iceworks-homepage',
-  'ice-website-homepage',
-  'ice-creator-landingpage',
-  'ice-open-platform-landingpage',
-
-  // icestark模板(2)
-  'ice-stark-layout',
-  'ice-stark-child',
+  // icestark模板
+  '@icedesign/stark-layout-scaffold',
+  '@icedesign/stark-child-scaffold',
 ];
 
 module.exports = scaffolds;
