46fed35217203605810039a806f5365296e56208
ClarkXia <xiawenwu41@gmail.com>
2019-12-25 14:29:39 +0800

    chore: migrate to build-scripts

--- scaffolds/ice-ts/build-plugin-local.js
+++ scaffolds/ice-ts/build-plugin-local.js
@@ -1,20 +1,5 @@
-const path = require('path');
-
-module.exports = {
-  entry: 'src/index.tsx',
-  publicPath: './',
-  alias: {
-    '@': path.resolve(__dirname, './src'),
-  },
-  plugins: [
-    ['ice-plugin-fusion', {
-      themePackage: '@icedesign/theme',
-    }],
-    ['ice-plugin-moment-locales', {
-      locales: ['zh-cn'],
-    }],
-  ],
-  chainWebpack: (config) => {
+module.exports = ({ onGetWebpackConfig }) => {
+  onGetWebpackConfig((config) => {
     // 修改对应 css module的 loader，默认修改 scss-module 同理可以修改 css-module 和 less-module 规则
     ['scss-module'].forEach((rule) => {
       if (config.module.rules.get(rule)) {
@@ -25,6 +10,5 @@ module.exports = {
         config.module.rule(rule).use('ts-css-module-loader').before('css-loader');
       }
     });
-  },
+  });
 };
-
