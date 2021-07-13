33a7d3cff023b0c30789b8999a3ea846920768d5
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2021-07-13 22:50:19 +0800

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 const { getESLintConfig } = require('@iceworks/spec');
 
 // https://www.npmjs.com/package/@iceworks/spec
-module.exports = getESLintConfig('react-ts', {
-  rules: {
-    'react/react-in-jsx-scope': 0,
-  },
-});
+module.exports = getESLintConfig('react-ts');
