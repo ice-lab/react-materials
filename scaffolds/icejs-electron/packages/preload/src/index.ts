689c381326dddd8638e09f3b1e5e22689a7dcf87
luhc228 <luhengchang228@gmail.com>
2021-12-23 10:46:46 +0800

    chore: fix lint

--- scaffolds/icejs-electron/packages/preload/src/index.ts
+++ scaffolds/icejs-electron/packages/preload/src/index.ts
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-  ['chrome', 'node', 'electron'].forEach(type => {
+  ['chrome', 'node', 'electron'].forEach((type) => {
     console.log(`${type}-version`, process.versions[type]);
   });
 });
