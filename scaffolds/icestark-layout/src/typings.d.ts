0da496b5e86b156106dc2cbbc2ce5b017d223604
大果 <sobear.me@gmail.com>
2021-12-28 15:26:26 +0800

    feat: remove midway scaffold & default lazy router (#196)
    * feat: remove midway scaffold & default lazy router
    
    * chore: icestark layout remove lazy

--- scaffolds/icestark-layout/src/typings.d.ts
+++ scaffolds/icestark-layout/src/typings.d.ts
@@ -1,13 +1,3 @@
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
