3cb280a49227d67d6ed23aa4165fdfa515ccc429
luhc228 <luhengchang228@126.com>
2022-05-31 17:47:45 +0800

    feat: update template

--- scaffolds/simple/src/pages/index.tsx
+++ scaffolds/simple/src/pages/index.tsx
@@ -1,33 +1,42 @@
-import Guide from '@/components/Guide';
+import { useState } from 'react';
+import './index.css';
+import logo from '@/assets/logo.png';
 
 const Home = () => {
-  return <Guide />;
+  const [count, setCount] = useState(0);
+  return (
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} alt="logo" className="logo" />
+        <p className="title">
+          ICE 3.0
+        </p>
+      </header>
+      <div className="body">
+        <button type="button" onClick={() => setCount((e) => e + 1)}>
+          🪂 Click me : {count}
+        </button>
+
+        <p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            href="https://ice.work/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ICE Docs
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
-
-export function getConfig() {
-  return {
-    title: 'Home',
-    meta: [
-      {
-        name: 'theme-color',
-        content: '#000',
-      },
-      {
-        name: 'title-color',
-        content: '#f00',
-      },
-    ]
-  };
-}
-
-export function getData() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        name: 'Home',
-      });
-    }, 1 * 100);
-  });
-}
 
 export default Home;
--- scaffolds/simple/src/typings.d.ts
+++ scaffolds/simple/src/typings.d.ts
@@ -2,3 +2,5 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.png';
