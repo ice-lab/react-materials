a6e16c2d2d20a0568e6f4a536a40cd44d10a0954
luhc228 <luhengchang228@126.com>
2022-12-03 14:02:30 +0800

    feat: exception page

--- scaffolds/ice3-fusion-pro/src/pages/success/index.tsx
+++ scaffolds/ice3-fusion-pro/src/pages/success/index.tsx
@@ -1,14 +1,27 @@
+import PageHeader from '@/components/PageHeader';
+import SuccessDetail from '@/components/SuccessDetail';
+import { ResponsiveGrid } from '@alifd/next';
+
+const { Cell } = ResponsiveGrid;
+
 export default function Success() {
   return (
-    // <Card bordered={false}>
-    //   <Result
-    //     status="success"
-    //     title="提交成功"
-    //     subTitle="提交结果页用于反馈一系列操作任务的处理结果， 如果仅是简单操作，使用 Message 全局提示反馈即可。 本文字区域可以展示简单的补充说明，如果有类似展示 “单据”的需求，下面这个灰色区域可以呈现比较复杂的内容。"
-    //     style={{ marginBottom: 16 }}
-    //     extra={extra}
-    //   />
-    // </Card>
-    <>Success</>
+    <ResponsiveGrid gap={20}>
+      <Cell colSpan={12}>
+        <PageHeader
+          title="成功页面"
+          breadcrumbs={[
+            { name: 'Feedback页面' },
+            { name: '结果页面' },
+            { name: '成功页面' },
+          ]}
+          description="成功页面描述"
+        />
+      </Cell>
+
+      <Cell colSpan={12}>
+        <SuccessDetail />
+      </Cell>
+    </ResponsiveGrid>
   );
 }
