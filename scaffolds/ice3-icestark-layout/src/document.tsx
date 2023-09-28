c572bca235f6e801119954c0f6352eaf0ceea1cb
luhc228 <luhengchang228@126.com>
2023-09-28 15:25:21 +0800

    feat: add viewport-fit to scaffolds

--- scaffolds/ice3-icestark-layout/src/document.tsx
+++ scaffolds/ice3-icestark-layout/src/document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="description" content="icestark framework scaffold" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <Meta />
         <Title />
         <Links />
